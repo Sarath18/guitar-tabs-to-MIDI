221fbb6be2b9514917fd5de490f07b6d071742ff
Sarath <sarathkrishnan99@gmail.com>
2018-08-06 19:29:34 +0530

    Encapsulated code into classes

--- read_tabs.py
+++ read_tabs.py
@@ -1,65 +1,59 @@
 #!/usr/bin/python3
+class Tabs(object):
 
-def displayTabs(tabs):
-    print("\n\n\n\n")
-    for i in range(len(tabs)):
-        for j in range(len(tabs[i])):
-            print(tabs[i][j],end="")
-        print()
+    def __init__(self,fname):
+        #Symbols to be removed to preprocess tabs
+        self.symbols = ['h','/','p','\\','*','^','|',"e","B","G","D","A","E"]
 
-def preprocess():
-    fname = "/home/sarath/tabs_to_midi/tab.txt"
-    symbols = ['h','/','p','\\','*','^','|',"e","B","G","D","A","E"]
-    with open(fname) as f:
-        content = f.readlines()
+        self.a = []
 
-    content = [x.strip() for x in content]
-    print("\n".join(content))
-    print("\n")
+        #MIDI equivalent of e B G D A E
+        self.notes = [64, 59, 55, 50, 45, 40]
+
+        self.fname = fname
+
+    def preprocess(self):
+        with open(self.fname) as f:
+            content = f.readlines()
+
+        content = [x.strip() for x in content]
+        print("\n".join(content))
+        print("\n")
 
 
-    for symbol in symbols:
+        for symbol in self.symbols:
+            for i,line in enumerate(content):
+                content[i] = line.replace(symbol,"-")
+
+        print("\n".join(content))
+
         for i,line in enumerate(content):
-            content[i] = line.replace(symbol,"-")
+            self.a.append(line.split(" "))
 
-    print("\n".join(content))
+        #print("\n\n\n\n")
 
-    a = []
+        for i in range(len(self.a)):
+            for j in range(len(self.a[i])):
+                #print(a[i][j],end="")
+                if self.a[i][j]=='1':
+                    if self.a[i][j+1] != '-':
+                        self.a[i][j] = str(((int(self.a[i][j])*10) + int(self.a[i][j+1])))
+                        self.a[i][j+1] = '-'
 
-    for i,line in enumerate(content):
-        a.append(line.split(" "))
+        self.displayTabs()
 
-    #print("\n\n\n\n")
-
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            #print(a[i][j],end="")
-            if a[i][j]=='1':
-                if a[i][j+1] != '-':
-                    a[i][j] = str(((int(a[i][j])*10) + int(a[i][j+1])))
-                    a[i][j+1] = '-'
-
-    displayTabs(a)
-
-    return a
+    def displayTabs(self):
+        print("\n\n\n\n")
+        for i in range(len(self.a[0])):
+            for j in range(len(self.a)):
+                print(self.a[j][i],end="")
+            print()
 
 
+    def convertNotes(self):
+        for i in range(len(self.a)):
+            for j in range(len(self.a[i])):
+                if self.a[i][j]!='-':
+                    self.a[i][j] = str(int(self.a[i][j])+notes[i])
 
-def convertNotes(a):
-    #MIDI equivalent of e B G D A E
-    notes = [64, 59, 55, 50, 45, 40]
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            if a[i][j]!='-':
-                a[i][j] = str(int(a[i][j])+notes[i])
-
-    displayTabs(a)
-
-
-def main():
-    a = preprocess()
-    convertNotes(a)
-
-
-if __name__ == '__main__':
-    main()
+        self.displayTabs()
